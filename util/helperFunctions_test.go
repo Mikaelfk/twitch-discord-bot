4cb2fb0ad3c1662c13beb0e9f8f953a7b2dff347
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-05-06 17:27:05 +0200

    actual fix mby

--- util/helperFunctions_test.go
+++ util/helperFunctions_test.go
@@ -1,5 +1,3 @@
-package util
-
 import (
 	"testing"
 )
@@ -8,9 +6,13 @@ func TestSearchByName(t *testing.T) {
 	channel := Channel{"en", "TestName", "testname", false, "Test Title", "Test Id", "Test Game", "Test Game Id", "Test Thumbnail", "Test Start Time"}
 	var data []struct{ Channel }
 	data = append(data, struct{ Channel }{channel})
-	twitchChannels := TwitchChannels{data} 
+	twitchChannels := TwitchChannels{data}
 	_, err := SearchByName("testname", twitchChannels)
 	if err != nil {
 		t.Errorf("Could not find testname among the channels")
 	}
+}
+
+func TestGetUserID(t *testing.T) {
+
 }

3aab27e0cfb0bcf719ac06e78febdfe70cee9714
Mikkel Aas <aa.mikkel2902@gmail.com>
2021-05-06 17:32:58 +0200

    Refactored all constants so that they start with URL not url to please the linter gods

--- constants/RESTConstants.go
+++ constants/RESTConstants.go
@@ -3,7 +3,6 @@ package constants
 
 // Often used constants
 const (
-<<<<<<< HEAD
 	URLTwitchChannelID   = "https://api.twitch.tv/helix/channels?broadcaster_id=" // + [id]
 	URLTwitchChannelName = "https://api.twitch.tv/helix/search/channels?query="   // + [broadcaster_name]
 	URLTwitchStream      = "https://www.twitch.tv/"                               // + [broadcaster_name]
@@ -13,17 +12,6 @@ const (
 	URLTwitchUserName    = "https://api.twitch.tv/helix/users?login="
 	URLTwitchFollowlist  = "https://api.twitch.tv/helix/users/follows?from_id="
 	URLTwitchGetTeams    = "https://api.twitch.tv/helix/teams?name=" // + [team name]
-=======
-	UrlTwitchChannelId   = "https://api.twitch.tv/helix/channels?broadcaster_id=" // + [id]
-	UrlTwitchChannelName = "https://api.twitch.tv/helix/search/channels?query="   // + [broadcaster_name]
-	UrlTwitchStream      = "https://www.twitch.tv/"                               // + [broadcaster_name]
-	UrlTwitchStreamInfo  = "https://api.twitch.tv/helix/streams"
-	UrlTwitchGames       = "https://api.twitch.tv/helix/search/categories?query=" // + [game_name]
-	UrlTwitchTopGames    = "https://api.twitch.tv/helix/games/top"
-	UrlTwitchUserName    = "https://api.twitch.tv/helix/users?login="
-	UrlTwitchFollowlist  = "https://api.twitch.tv/helix/users/follows?from_id="
-	UrlTwitchGetTeams    = "https://api.twitch.tv/helix/teams?name="			  // + [team name]
->>>>>>> Added a rest constant for the get twitch team url
 
 	TwitchAPIResolution     = "52x72.jpg"
 	DiscordBotImgResolution = "200x150"

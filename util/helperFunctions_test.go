f716ef5e17a833259ebd9f7c677dff1be165d767
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-05-06 17:14:42 +0200

    Fixed TestSearchByName

--- util/helperFunctions_test.go
+++ util/helperFunctions_test.go
@@ -4,13 +4,12 @@ import (
 	"testing"
 )
 
-
 func TestSearchByName(t *testing.T) {
 	channel := Channel{"en", "TestName", "testname", false, "Test Title", "Test Id", "Test Game", "Test Game Id", "Test Thumbnail", "Test Start Time"}
 	var data []struct{Channel}
 	data = append(data, struct{Channel}{channel})
 	twitchChannels := TwitchChannels{data} 
-	channel, err := SearchByName("testname", twitchChannels)
+	_, err := SearchByName("testname", twitchChannels)
 	if err != nil {
 		t.Errorf("Could not find testname among the channels")
 	}

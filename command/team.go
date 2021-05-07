b20333fb2cc21a07de94f75009f57897f1ba12ac
Mikkel Aas <aa.mikkel2902@gmail.com>
2021-05-07 02:08:04 +0200

    The team.go command handler now handles input parameters from the user.

--- command/team.go
+++ command/team.go
@@ -1,36 +1,68 @@
 package command
 
 import (
+	"fmt"
+	"log"
+	"twitch-discord-bot/constants"
 	"twitch-discord-bot/twitchapi"
 	"twitch-discord-bot/util"
 
 	"github.com/bwmarrin/discordgo"
 )
 
-//
-// Example command
-//
-
 var (
 	// define name and description for command
 	teamCommand = discordgo.ApplicationCommand{
 		Name:        "team",
-		Description: "will retrieve info about a twitch team",
+		Description: "Will retrieve info about a twitch team",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "team-name",
+				Description: "The name of the team",
+				Required:    true,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionBoolean,
+				Name:        "is-live",
+				Description: "Boolean for live",
+				Required:    false,
+			},
+		},
 	}
 
 	// define commandHandler for this command
 	teamCommandHandler = func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 
-		teamName, _ := twitchapi.GetTeamName("luminosity")
-		members, _ := twitchapi.GetAllTeamMembers("luminosity")
+		var members []string
+
+		teamName := fmt.Sprintf("%v", i.Data.Options[0].Value)
+		isLive := i.Data.Options[1].BoolValue()
+
+		print(teamName)
+		print(isLive)
+
+		exist, err := twitchapi.TeamExist(teamName)
+		if err != nil {
+			log.Println("unexpected error")
+			return
+		}
+
+		if !exist {
+			util.DiscordBotResponder(constants.BotNoResultsMsg, s, i)
+		}
+
+		if isLive {
+			members, _ = twitchapi.GetLiveTeamMembers(teamName)
+		} else {
+			members, _ = twitchapi.GetAllTeamMembers(teamName)
+		}
 
 		responseString := "Team name: " + teamName + "\n"
-
 		responseString += "Team members: \n"
 		for _, member := range members {
 			responseString += member + ", "
 		}
-
 		util.DiscordBotResponder(responseString, s, i)
 	}
 )
